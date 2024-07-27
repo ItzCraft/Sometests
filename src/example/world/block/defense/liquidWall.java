82c24503dc487fb14343a6ac50b0c339820cb2c1
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-07-27 22:42:27 +0300

    Create liquidWall.java

---
+++ src/example/world/block/defense/liquidWall.java
@@ -0,0 +1,37 @@
+package example.world.block.defense;
+import mindustry.gen.*;
+import mindustry.world.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.type.*;
+
+public class LiquidWall extends Wall {
+    public float liquidMultiplier = 1.5f;
+    public Liquid liquidType = Liquids.water;
+    
+    public LiquidWall(String name) {
+        super(name);
+        update = true;
+    }
+    
+    @Override
+    public void setStats() {
+        super.setStats();
+        stats.add(Stat.health, (health * liquidMultiplier), StatUnit.none);
+    }
+    
+    @Override
+    public void updateTile() {
+        super.updateTile(){
+        if(liquidCapacity > 0 && liquids.get(liquidType) > 0) {
+            health = baseHealth * liquidMultiplier;
+        } else {
+            health = baseHealth;
+        }
+    }
+    
+    @Override
+    public void init() {
+        super.init();
+        liquidCapacity = 10f;
+    }
+}
