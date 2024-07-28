c5352ac3fc4f5bd33ad11706672912ca7a7f8b5a
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-07-28 07:14:57 +0300

    Update Tblocks.java

--- src/example/content/Tblocks.java
+++ src/example/content/Tblocks.java
@@ -1,15 +1,16 @@
 package example.content;
 import mindustry.type.*;
 import mindustry.world.*;
-import mindustry.content.*;
-import example.world.block.*;
+import mindustry.content.Blocks;
+import mindustry.content.Items;
+import example.world.block.liquidWall;
 
 public class Tblocks {
   public static Block testWall;
 
   public static void load() {
     testWall = new liquidWall("liquid-wall"){{
-      requirements(Category.defense, ItemStack.with(Items.copper, 6, Items.lead, 4));
+      requirements(Category.defense, with(Items.copper, 6, Items.lead, 4));
       health = 200;
       size = 2;
     }};
